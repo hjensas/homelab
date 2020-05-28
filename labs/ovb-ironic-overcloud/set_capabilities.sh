b4b728e45eff98152615e3fb39358918117dc165
Harald Jensås <hjensas@redhat.com>
2020-05-28 22:42:06 +0200

    Use default from ovb in capabilities flavor

--- labs/ovb-ironic-overcloud/set_capabilities.sh
+++ labs/ovb-ironic-overcloud/set_capabilities.sh
@@ -3,7 +3,7 @@ source ~/stackrc
 openstack flavor create \
   --disk 40 --public --ram 4096 --vcpus 1 --rxtx-factor 1.0 \
   --property capabilities:boot_option='local' \
-  --property capabilities:profile='ironic-conductor-a' \
+  --property capabilities:profile='leaf1' \
   --property resources:CUSTOM_BAREMETAL='1' \
   --property resources:DISK_GB='0' \
   --property resources:MEMORY_MB='0' \
@@ -12,7 +12,7 @@ openstack flavor create \
 openstack flavor create \
   --disk 40 --public --ram 4096 --vcpus 1 --rxtx-factor 1.0 \
   --property capabilities:boot_option='local' \
-  --property capabilities:profile='ironic-conductor-b' \
+  --property capabilities:profile='leaf2' \
   --property resources:CUSTOM_BAREMETAL='1' \
   --property resources:DISK_GB='0' \
   --property resources:MEMORY_MB='0' \
@@ -21,13 +21,6 @@ openstack flavor create \
 
 openstack baremetal node set \
   --property capabilities='profile:control,boot_option:local' baremetal-0
-
-openstack baremetal node set \
- --property capabilities='profile:ironic-conductor-a,boot_option:local' baremetal-leaf1-0
-
-openstack baremetal node set \
-  --property capabilities='profile:ironic-conductor-b,boot_option:local' baremetal-leaf2-0
-
 
 #
 # Delete nodes on undercloud, will add them in overcloud.
