5cf11c340a38221a59a3cca3c55f34444810c192
Harald Jensås <hjensas@redhat.com>
2020-09-23 18:28:19 +0200

    Fix ipv6-routed lab deploy script

--- labs/ovb-ipv6-routed/ovb/deploy_ovb.sh
+++ labs/ovb-ipv6-routed/ovb/deploy_ovb.sh
@@ -1,20 +1,21 @@
 # Deploy ovb lab
 ovb-deploy \
  --env env-routed-lab.yaml \
- --id ipv6-routed
  --quintupleo \
+ --name quintupleo-ipv6-l3
+ --id ipv6-l3 \
  --env environments/all-networks.yaml \
  --env env-ipv6-routed.yaml \
- --env env-ipv6-role-leaf1.yaml \
- --env env-ipv6-role-leaf2.yaml
+ --role env-ipv6-role-leaf1.yaml \
+ --role env-ipv6-role-leaf2.yaml
 
 # Build nodes json
 ./bin/build-nodes-json \
 	--env env-routed-lab.yaml \
 	--physical_network
 
-OVB_UNDERCLOUD=$(openstack stack show quintupleo -f json -c outputs | jq '.outputs[0].output_value' | sed s/'"'//g)
-OVB_UNDERCLOUD_PUBLIC=$(openstack server show undercloud -f json -c addresses | jq '.addresses' | sed s/.*public=// | sed s/\;.*// | sed s/'"'//g)
+OVB_UNDERCLOUD=$(openstack stack show quintupleo-ipv6-l3 -f json -c outputs | jq '.outputs[0].output_value' | sed s/'"'//g)
+OVB_UNDERCLOUD_PUBLIC=$(openstack server show undercloud-ipv6-l3 -f json -c addresses | jq '.addresses' | sed s/.*public=// | sed s/\;.*// | sed s/'"'//g)
 cat << EOF > inventory.ini
 [undercloud]
 $OVB_UNDERCLOUD ansible_user=centos ansible_ssh_extra_args='-o StrictHostKeyChecking=no' undercloud_public_ip=$OVB_UNDERCLOUD_PUBLIC
@@ -24,7 +25,7 @@ ansible-playbook -i inventory.ini ../homelab/labs/playbooks/ssh_hardening.yaml
 
 scp -o StrictHostKeyChecking=no nodes.json centos@$OVB_UNDERCLOUD:~/instackenv.json
 
-DEPLOY_UNDERCLOUD="ansible-playbook -i inventory.ini ../homelab/labs/ovb-ironic-overcloud/playbooks//deploy_undercloud.yaml"
+DEPLOY_UNDERCLOUD="ansible-playbook -i inventory.ini ../homelab/labs/ovb-ipv6-routed/playbooks//deploy_undercloud.yaml"
 DEPLOY_OVERCLOUD="Log into undercloud ($OVB_UNDERCLOUD) and run command: bash ~/overcloud/deploy_overcloud.sh"
 echo "###############################################"
 echo -e "Undercloud floating IP:\n\t$OVB_UNDERCLOUD"
