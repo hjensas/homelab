65061b69cc43e7d8381715dca6f55596352d51b3
Harald Jensås <hjensas@redhat.com>
2021-01-12 17:40:54 +0100

    deploy command, node_data, role_data etc.

---
+++ labs/ovb-ipv4-tls-e-routed/overcloud/deploy_overcloud.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source /home/cloud-user/stackrc
+cd /home/cloud-user
+
+openstack overcloud deploy --templates /home/centos/tripleo-heat-templates \
+  -n /home/centos/overcloud/network_data.yaml \
+  -r /home/centos/overcloud/roles_data.yaml \
+  -e /home/centos/tripleo-heat-templates/environments/enable-swap.yaml \
+  -e /home/centos/tripleo-heat-templates/environments/network-isolation.yaml \
+  -e /home/centos/tripleo-heat-templates/environments/ssl/enable-internal-tls.yaml \
+  -e /home/centos/tripleo-heat-templates/environments/ssl/tls-everywhere-endpoints-dns.yaml \
+  -e /home/centos/overcloud/node_data.yaml
+
