d475ec3c7cf78fdc919717a87857113af85611f7
Harald Jensås <hjensas@redhat.com>
2021-01-12 21:21:51 +0100

    Fix paths

--- labs/ovb-ipv4-tls-e-routed/overcloud/deploy_overcloud.sh
+++ labs/ovb-ipv4-tls-e-routed/overcloud/deploy_overcloud.sh
@@ -3,13 +3,13 @@
 source /home/cloud-user/stackrc
 cd /home/cloud-user
 
-openstack overcloud deploy --templates /home/centos/tripleo-heat-templates \
-  -n /home/centos/overcloud/network_data.yaml \
-  -r /home/centos/overcloud/roles_data.yaml \
-  -e /home/centos/tripleo-heat-templates/environments/enable-swap.yaml \
-  -e /home/centos/tripleo-heat-templates/environments/network-isolation.yaml \
-  -e /home/centos/tripleo-heat-templates/environments/network-environment.yaml \
-  -e /home/centos/tripleo-heat-templates/environments/ssl/enable-internal-tls.yaml \
-  -e /home/centos/tripleo-heat-templates/environments/ssl/tls-everywhere-endpoints-dns.yaml \
-  -e /home/centos/overcloud/node_data.yaml
+openstack overcloud deploy --templates /home/cloud-user/tripleo-heat-templates \
+  -n /home/cloud-user/overcloud/network_data.yaml \
+  -r /home/cloud-user/overcloud/roles_data.yaml \
+  -e /home/cloud-user/tripleo-heat-templates/environments/enable-swap.yaml \
+  -e /home/cloud-user/tripleo-heat-templates/environments/network-isolation.yaml \
+  -e /home/cloud-user/tripleo-heat-templates/environments/network-environment.yaml \
+  -e /home/cloud-user/tripleo-heat-templates/environments/ssl/enable-internal-tls.yaml \
+  -e /home/cloud-user/tripleo-heat-templates/environments/ssl/tls-everywhere-endpoints-dns.yaml \
+  -e /home/cloud-user/overcloud/node_data.yaml
 
