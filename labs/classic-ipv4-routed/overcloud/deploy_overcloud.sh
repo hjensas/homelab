3a5225ac2a26ca7e7df0b9220a295372d8fe59b2
Harald Jensås <hjensas@redhat.com>
2021-02-12 04:18:04 +0100

    Add classic-ipv4-routed lab
    This deploys with networking in Heat etc

---
+++ labs/classic-ipv4-routed/overcloud/deploy_overcloud.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+source /home/centos/stackrc
+cd /home/centos
+
+openstack overcloud deploy --templates /home/centos/tripleo-heat-templates \
+  --deployed-server \
+  -n /home/centos/overcloud/network_data_v1.yaml \
+  -r /home/centos/overcloud/my_roles_data.yaml \
+  -e /home/centos/tripleo-heat-templates/environments/deployed-server-environment.yaml \
+  -e /home/centos/overcloud-baremetal-deployed.yaml \
+  -e /home/centos/tripleo-heat-templates/environments/enable-swap.yaml \
+  -e /home/centos/tripleo-heat-templates/environments/network-isolation.yaml \
+  -e /home/centos/tripleo-heat-templates/environments/net-multiple-nics.yaml \
+  -e /home/centos/overcloud/node_data.yaml
+
+
