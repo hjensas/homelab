79600acb8a37102cea630e4502a33e8ebbdb22db
Harald Jensås <hjensas@redhat.com>
2021-03-08 03:09:51 +0100

    Set --network-ports option in bm deploy command

--- labs/ovb-ipv4-routed/overcloud/deploy_baremetal.sh
+++ labs/ovb-ipv4-routed/overcloud/deploy_baremetal.sh
@@ -2,6 +2,7 @@ source /home/centos/stackrc
 
 openstack overcloud node provision \
 	--stack overcloud \
+	--network-ports \
 	--output ~/overcloud-baremetal-deployed.yaml \
 	~/overcloud/baremetal_deployment.yaml
 
