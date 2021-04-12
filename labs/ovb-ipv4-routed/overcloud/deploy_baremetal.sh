e62441f385dd9f9c8cdfc867e4ddb7aae8dcbf04
Harald Jensås <hjensas@redhat.com>
2021-04-12 17:38:58 +0200

    Add --network-config flag to baremetal deploy

--- labs/ovb-ipv4-routed/overcloud/deploy_baremetal.sh
+++ labs/ovb-ipv4-routed/overcloud/deploy_baremetal.sh
@@ -3,6 +3,7 @@ source /home/centos/stackrc
 openstack overcloud node provision \
 	--stack overcloud \
 	--network-ports \
+	--network-config \
 	--output ~/overcloud-baremetal-deployed.yaml \
 	~/overcloud/baremetal_deployment.yaml
 
