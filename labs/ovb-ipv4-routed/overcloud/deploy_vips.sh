c9827bea573efe3938ee529c505e2b6017ee2edd
Harald Jensås <hjensas@redhat.com>
2021-05-10 13:40:58 +0200

    Fix deploy vips script - missing bacslash line break escape

--- labs/ovb-ipv4-routed/overcloud/deploy_vips.sh
+++ labs/ovb-ipv4-routed/overcloud/deploy_vips.sh
@@ -4,7 +4,7 @@ source /home/centos/stackrc
 cd /home/centos
 
 openstack overcloud network vip provision \
-  --output /home/centos/overcloud-vips-deployed.yaml
+  --output /home/centos/overcloud-vips-deployed.yaml \
   --stack overcloud \
   /home/centos/overcloud/vip_data.yaml
 
