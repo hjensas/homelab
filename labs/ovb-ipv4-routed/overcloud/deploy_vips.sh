435eeccfe72dee0a5d22bc1865af03acf0492ce2
Harald Jensås <hjensas@redhat.com>
2021-05-10 13:39:55 +0200

    Add vips_data.yaml and deploy script for oc vips

---
+++ labs/ovb-ipv4-routed/overcloud/deploy_vips.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+source /home/centos/stackrc
+cd /home/centos
+
+openstack overcloud network vip provision \
+  --output /home/centos/overcloud-vips-deployed.yaml
+  --stack overcloud \
+  /home/centos/overcloud/vip_data.yaml
+
