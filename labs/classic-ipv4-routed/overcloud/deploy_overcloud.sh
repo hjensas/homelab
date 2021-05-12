7dbb6adfd0953285f1f749f9254e44a39e968fd2
Harald Jensås <hjensas@redhat.com>
2021-05-12 14:47:48 +0200

    Make classic lab use nova

--- labs/classic-ipv4-routed/overcloud/deploy_overcloud.sh
+++ labs/classic-ipv4-routed/overcloud/deploy_overcloud.sh
@@ -7,8 +7,6 @@ openstack overcloud deploy --templates /home/centos/tripleo-heat-templates \
   --deployed-server \
   -n /home/centos/overcloud/network_data_v1.yaml \
   -r /home/centos/overcloud/my_roles_data.yaml \
-  -e /home/centos/tripleo-heat-templates/environments/deployed-server-environment.yaml \
-  -e /home/centos/overcloud-baremetal-deployed.yaml \
   -e /home/centos/tripleo-heat-templates/environments/enable-swap.yaml \
   -e /home/centos/tripleo-heat-templates/environments/network-isolation.yaml \
   -e /home/centos/tripleo-heat-templates/environments/net-multiple-nics.yaml \
