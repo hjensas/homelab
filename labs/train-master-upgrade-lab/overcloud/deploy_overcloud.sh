1b14979e084e4c0cec928875b06b4f2cfea3bbf1
Harald Jensås <hjensas@redhat.com>
2021-06-02 00:26:48 +0200

    Fix typo

--- labs/train-master-upgrade-lab/overcloud/deploy_overcloud.sh
+++ labs/train-master-upgrade-lab/overcloud/deploy_overcloud.sh
@@ -6,10 +6,10 @@ cd /home/centos
 openstack overcloud deploy --templates \
   -n /home/centos/overcloud/network_data_v1.yaml \
   -r /home/centos/overcloud/my_roles_data.yaml \
-  -e /usr/share/openstack-tripleo-heat-tempaltes/environments/enable-swap.yaml \
-  -e /usr/share/openstack-tripleo-heat-tempaltes/environments/network-isolation.yaml \
-  -e /usr/share/openstack-tripleo-heat-tempaltes/environments/network-environment.yaml \
-  -e /usr/share/openstack-tripleo-heat-tempaltes/environments/net-multiple-nics.yaml \
-  -e /usr/share/openstack-tripleo-heat-tempaltes/environments/services/zaqar.yaml
+  -e /usr/share/openstack-tripleo-heat-templates/environments/enable-swap.yaml \
+  -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
+  -e /usr/share/openstack-tripleo-heat-templates/environments/network-environment.yaml \
+  -e /usr/share/openstack-tripleo-heat-templates/environments/net-multiple-nics.yaml \
+  -e /usr/share/openstack-tripleo-heat-templates/environments/services/zaqar.yaml
   -e /home/centos/overcloud/node_data.yaml
 
