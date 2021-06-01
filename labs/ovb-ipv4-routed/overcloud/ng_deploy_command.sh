49c4d395d5220f6be3b4d99d6119d2c2ac6515c0
Harald Jensås <hjensas@redhat.com>
2021-06-01 02:08:12 +0200

    Add NG oc deploy command

---
+++ labs/ovb-ipv4-routed/overcloud/ng_deploy_command.sh
@@ -0,0 +1,12 @@
+openstack overcloud deploy \
+  --stack ng_overcloud \
+  --templates \
+  --networks-file ~/overcloud/network_data_v2.yaml \
+  --vips-file ~/overcloud/vip_data.yaml \
+  --baremetal-deployment ~/overcloud/baremetal_deployment.yaml \
+  --network-config \
+  --deployed-server \
+  -e /home/centos/tripleo-heat-templates/environments/enable-swap.yaml \
+  -e /home/centos/tripleo-heat-templates/environments/net-multiple-nics.yaml \
+  -e /home/centos/overcloud/node_data.yaml
+
