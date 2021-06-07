8ed9025d42e3ef3b5f63474751017b82f3970aa6
Harald Jensås <hjensas@redhat.com>
2021-06-07 14:39:19 +0200

    drop --network-ports, there is --network-config

--- labs/ovb-ipv4-routed/overcloud/deploy_baremetal.sh
+++ labs/ovb-ipv4-routed/overcloud/deploy_baremetal.sh
@@ -2,7 +2,6 @@ source /home/centos/stackrc
 
 openstack overcloud node provision \
 	--stack overcloud \
-	--network-ports \
 	--network-config \
 	--output ~/overcloud-baremetal-deployed.yaml \
 	~/overcloud/baremetal_deployment.yaml
