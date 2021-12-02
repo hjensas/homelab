e9b88e869622572dbe81f84364c3f14f9e663208
Harald Jensås <hjensas@redhat.com>
2021-12-02 19:06:09 +0100

    Fix typo in option name

--- labs/ovb-ipv4-routed/overcloud/deploy_baremetal.sh
+++ labs/ovb-ipv4-routed/overcloud/deploy_baremetal.sh
@@ -3,7 +3,7 @@ source /home/centos/stackrc
 openstack overcloud node provision \
 	--stack my_overcloud \
 	--network-config \
-	--tempaltes ~/tripleo-heat-templates \
+	--templates ~/tripleo-heat-templates \
 	--output ~/overcloud-baremetal-deployed.yaml \
 	~/overcloud/baremetal_deployment.yaml
 
