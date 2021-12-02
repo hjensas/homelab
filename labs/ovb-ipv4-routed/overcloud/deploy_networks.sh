e2d77b0a6b4f74b64c3bc4a46144256734ed3b4b
Harald Jensås <hjensas@redhat.com>
2021-12-02 19:09:02 +0100

    Add --template commands instead of running sed on post

--- labs/ovb-ipv4-routed/overcloud/deploy_networks.sh
+++ labs/ovb-ipv4-routed/overcloud/deploy_networks.sh
@@ -5,8 +5,7 @@ cd /home/centos
 
 
 openstack overcloud network provision \
+  --templates ~/tripleo-heat-templates \
   --output /home/centos/overcloud-networks-deployed.yaml \
   /home/centos/overcloud/network_data_v2.yaml 
-
-sed -i 's|/usr/share/openstack-tripleo-heat-templates|/home/centos/tripleo-heat-templates|g' /home/centos/overcloud-networks-deployed.yaml
 
--- labs/ovb-ipv4-routed/overcloud/deploy_vips.sh
+++ labs/ovb-ipv4-routed/overcloud/deploy_vips.sh
@@ -4,9 +4,8 @@ source /home/centos/stackrc
 cd /home/centos
 
 openstack overcloud network vip provision \
-  --output /home/centos/overcloud-vips-deployed.yaml \
   --stack my_overcloud \
+  --templates ~/tripleo-heat-templates \
+  --output /home/centos/overcloud-vips-deployed.yaml \
   /home/centos/overcloud/vip_data.yaml
-
-sed -i 's|/usr/share/openstack-tripleo-heat-templates|/home/centos/tripleo-heat-templates|g' /home/centos/overcloud-vips-deployed.yaml
 
