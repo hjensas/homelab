a7dbe8301b24dd88c8f80b78ebe39c6a94023282
Harald Jensås <hjensas@redhat.com>
2021-12-02 19:05:18 +0100

    minor changes

--- labs/ovb-ipv4-routed/overcloud/deploy_baremetal.sh
+++ labs/ovb-ipv4-routed/overcloud/deploy_baremetal.sh
@@ -3,7 +3,7 @@ source /home/centos/stackrc
 openstack overcloud node provision \
 	--stack my_overcloud \
 	--network-config \
+	--tempaltes ~/tripleo-heat-templates \
 	--output ~/overcloud-baremetal-deployed.yaml \
 	~/overcloud/baremetal_deployment.yaml
 
-sed -i 's|/usr/share/openstack-tripleo-heat-templates|/home/centos/tripleo-heat-templates|g' /home/centos/overcloud-baremetal-deployed.yaml
