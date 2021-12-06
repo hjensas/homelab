897c48ee906485bd9aa7a947cb58df886941eb57
Harald Jensås <hjensas@redhat.com>
2021-12-06 22:58:51 +0100

    DELETE IPv6 Train

--- labs/ovb-ipv6-routed-train/set_capabilities.sh
+++
@@ -1,7 +0,0 @@
-source ~/stackrc
-
-openstack baremetal node set \
-  --property capabilities='profile:control,boot_option:local' baremetal-ipv6-l3-leaf1-0
-openstack baremetal node set \
-  --property capabilities='profile:compute-leaf2,boot_option:local' baremetal-ipv6-l3-leaf2-0
-
