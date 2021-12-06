f7c0a3fc1738b9154db3e35b0c138685d81e6a78
Harald Jensås <hjensas@redhat.com>
2021-12-06 23:07:15 +0100

    IPv6 Train

---
+++ labs/ovb-train-ipv6-routed/overcloud/deploy_overcloud.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source /home/centos/stackrc
+cd /home/centos
+
+openstack overcloud deploy --stack my_overcloud \
+  --templates \
+  -n /home/centos/overcloud/network_data.yaml \
+  -r /home/centos/overcloud/my_roles_data.yaml \
+  -e /usr/share/openstack-tripleo-heat-templates/environments/enable-swap.yaml \
+  -e /usr/share/openstack-tripleo-heat-templates/environments/net-multiple-nics.yaml \
+  -e /home/centos/overcloud/node_data.yaml
+
