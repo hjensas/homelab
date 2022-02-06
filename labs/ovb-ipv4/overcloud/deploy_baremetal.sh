ab21fa19d8884539cfb8c38956479a884c8fc19a
Harald Jensås <hjensas@redhat.com>
2022-02-06 01:36:11 +0100

    Fix typo

--- labs/ovb-ipv4/overcloud/deploy_baremetal.sh
+++ labs/ovb-ipv4/overcloud/deploy_baremetal.sh
@@ -3,6 +3,6 @@ source /home/centos/stackrc
 openstack overcloud node provision \
   --stack overcloud \
   --output ~/overcloud-baremetal-deployed.yaml \
-  --templats /home/centos/tripleo-heat-templates \
+  --templates /home/centos/tripleo-heat-templates \
   ~/overcloud/baremetal_deployment.yaml
 
