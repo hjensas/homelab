8706ec764a81baffe66b16cb1a2285e3bb0d45bf
Harald Jensås <hjensas@redhat.com>
2022-02-11 00:17:00 +0100

    s/centos/cloud-user cs9 lab

--- labs/ovb-ipv4-cs9/overcloud/deploy_baremetal.sh
+++ labs/ovb-ipv4-cs9/overcloud/deploy_baremetal.sh
@@ -1,7 +1,8 @@
-source /home/centos/stackrc
+source /home/cloud-user/stackrc
 
 openstack overcloud node provision \
-	--stack overcloud \
-	--output ~/overcloud-baremetal-deployed.yaml \
-	~/overcloud/baremetal_deployment.yaml
+  --stack overcloud \
+  --templates /home/cloud-user/tripleo-heat-templates \
+  --output ~/overcloud-baremetal-deployed.yaml \
+  ~/overcloud/baremetal_deployment.yaml
 
--- labs/ovb-ipv4-cs9/overcloud/deploy_overcloud.sh
+++ labs/ovb-ipv4-cs9/overcloud/deploy_overcloud.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-source /home/centos/stackrc
-cd /home/centos
+source /home/cloud-user/stackrc
+cd /home/cloud-user
 
-openstack overcloud deploy --templates /home/centos/tripleo-heat-templates \
+openstack overcloud deploy --templates /home/cloud-user/tripleo-heat-templates \
   --deployed-server \
   --disable-validations \
-  -n /home/centos/overcloud/network_data_v2.yaml \
-  -r /home/centos/overcloud/my_roles_data.yaml \
-  -e /home/centos/tripleo-heat-templates/environments/deployed-server-environment.yaml \
-  -e /home/centos/overcloud-baremetal-deployed.yaml \
-  -e /home/centos/overcloud-networks-deployed.yaml \
-  -e /home/centos/tripleo-heat-templates/environments/enable-swap.yaml \
-  -e /home/centos/tripleo-heat-templates/environments/network-isolation.yaml \
-  -e /home/centos/tripleo-heat-templates/environments/net-multiple-nics.yaml \
-  -e /home/centos/overcloud/node_data.yaml
+  -n /home/cloud-user/overcloud/network_data_v2.yaml \
+  -r /home/cloud-user/overcloud/my_roles_data.yaml \
+  -e /home/cloud-user/tripleo-heat-templates/environments/deployed-server-environment.yaml \
+  -e /home/cloud-user/overcloud-baremetal-deployed.yaml \
+  -e /home/cloud-user/overcloud-networks-deployed.yaml \
+  -e /home/cloud-user/tripleo-heat-templates/environments/enable-swap.yaml \
+  -e /home/cloud-user/tripleo-heat-templates/environments/network-isolation.yaml \
+  -e /home/cloud-user/tripleo-heat-templates/environments/net-multiple-nics.yaml \
+  -e /home/cloud-user/overcloud/node_data.yaml
 
 
