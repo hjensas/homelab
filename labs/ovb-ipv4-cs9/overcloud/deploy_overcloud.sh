ca862e07e854ce8d23a652dfb2bbc727e2db5074
Harald Jensås <hjensas@redhat.com>
2022-06-11 01:10:50 +0200

    update bm deployment yaml

--- labs/ovb-ipv4-cs9/overcloud/deploy_overcloud.sh
+++ labs/ovb-ipv4-cs9/overcloud/deploy_overcloud.sh
@@ -13,7 +13,6 @@ openstack overcloud deploy --templates /home/cloud-user/tripleo-heat-templates \
   -e /home/cloud-user/overcloud-networks-deployed.yaml \
   -e /home/cloud-user/overcloud-vips-deployed.yaml \
   -e /home/cloud-user/tripleo-heat-templates/environments/enable-swap.yaml \
-  -e /home/cloud-user/tripleo-heat-templates/environments/network-isolation.yaml \
   -e /home/cloud-user/tripleo-heat-templates/environments/net-multiple-nics.yaml \
   -e /home/cloud-user/overcloud/node_data.yaml
 
